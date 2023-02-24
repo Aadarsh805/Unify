3b7c0172835864c275aed44008516769cb8f5688
aadarsh805 <aadarshthakur80501@gmail.com>
2023-02-24 22:08:19 +0530

    build: installed dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return <h1 className="text-4xl bg-300 p-4">hello!</h1>;
 }
