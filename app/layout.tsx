43700ea9242227521429f283dea48842381594d2
manish bisht <manish205770@keshav.du.ac.in>
2023-02-25 15:43:32 +0530

    fix: merge conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { noto_serif } from "@/public/assets/fonts/font";
 import supabase from "@/server/supabase";
 import { useEffect } from "react";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import Head from "./head";
 
@@ -10,20 +12,42 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    const funct = async () => {
-      const { data } = await supabase.auth.getUser();
-      const { id }: any = data?.user as any;
-      console.log(id);
-    };
+  const onSignIn = async () => {
+    const { data } = await supabase.auth.getUser();
+    const { id }: any = data?.user as any;
+    console.log(id);
+  };
+  // useEffect(() => {
 
-    funct();
+  //   funct();
+  // }, []);
+
+  // auth-change event
+  const observeAuthChange = async () => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((e, session) => {
+      if (e === "SIGNED_IN") onSignIn();
+    });
+    subscription.callback("SIGNED_IN", null);
+  };
+
+  useEffect(() => {
+    observeAuthChange();
   }, []);
 
   return (
     <html lang="en">
       <Head />
-      <body>{children}</body>
+      <body
+        style={{
+          fontFamily: `${noto_serif.className}`,
+        }}
+        className="bg-[#F4F1E7]"
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
