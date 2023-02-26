6b16b564fadf89c2bbf5b3789f02708137278048
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-26 15:34:35 +0530

    feat: added matched

--- app/account/match/page.tsx
+++ app/account/match/page.tsx
@@ -1,33 +1,54 @@
 "use client";
 
+import useStore from "@/app/store/store";
 import supabase from "@/server/supabase";
+import Link from "next/link";
 import { useEffect, useState } from "react";
-import MatchCard from "./MatchCard";
 
 const MatchPage = () => {
   const [myMatches, setMyMatches] = useState<any>([]);
 
+  const { userProfile } = useStore((state: any) => {
+    return {
+      userProfile: state.userProfile,
+    };
+  });
+
   useEffect(() => {
     const getMyInterested = async () => {
-      const { data: myData } = await supabase.auth.getUser();
-      const { id: myId }: any = myData.user;
-
       //getting all the products that i interested
+
       const { data: myMatches } = await supabase
         .from("match")
-        .select("matched_user");
+        .select("*,users(id,username)")
+        .eq("owner_id", userProfile.id);
+
+      // console.log(myMatches, "datafghfg");
+
+      if (myMatches) {
+        console.log(myMatches);
+
+        const users = new Set(myMatches.map((match) => match.users));
+        setMyMatches(Array.from(users));
+      }
     };
     getMyInterested();
-  }, []);
+  }, [userProfile.id]);
 
   console.log(myMatches, "mymathces");
 
   return (
     <div className="px-10">
-      <h1 className="mb-10 text-2xl font-bold">Match Products</h1>
-      <div className="flex flex-wrap items-center gap-20">
+      <h1 className="mb-10 text-2xl font-bold">Interest matches with</h1>
+      <div className="grid grid-cols-4 gap-4 sm:grid-cols-8">
         {myMatches.map((match: any) => (
-          <MatchCard key={match} />
+          <>
+            <div className="rounded-md border border-slate-700 px-4 py-1 font-medium capitalize text-slate-800">
+              <Link href={`/user/${match.id}`} className="capitalize">
+                {match.username}
+              </Link>
+            </div>
+          </>
         ))}
       </div>
     </div>
